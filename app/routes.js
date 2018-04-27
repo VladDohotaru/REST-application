d98a97327fc6187ddbce5077e5fd7278a3e35869
vldo <vldo@netop.com>
2018-04-27 18:27:17 +0300

    added .gitignore and solve conflicts

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,4 @@
 'use strict';
-<<<<<<< HEAD
 
 const Model = require('../config/models/model.js');
 const promise = Model.Product;
@@ -210,149 +209,11 @@ module.exports = (router, passport) => {
   });
 
   router.get('/logout', (req, res) => {
-=======
-const connection = require('../config/db');
-module.exports = (app, passport) => {
-
-  app.get('/', (req, res) => {
-    res.render('index.ejs');
-  });
-
-  app.get('/signup', (req, res) => {
-    res.render('signup.ejs');
-  });
-
-  app.get('/login', (req, res) => {
-    res.render('login.ejs');
-  });
-
-  function requireLogin (req, res, next) {
-    if (!req.user) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-
-  app.get('/profile', requireLogin, (req, res) => {
-    res.render('profile.ejs');
-  });
-  app.use( (req, res, next) => {
-    if (req.session && req.session.user) {
-      connection.query('SELECT * FROM users WHERE username = ?', [req.session.user.email], (err, user) => {
-        if (err) {
-          console.log('Aici crapa ' + err);
-          return;
-        }
-        if (user) {
-          req.user = user;
-          delete req.user.password;
-          req.session.user = user;
-
-          // req.locals.user = user;
-        }
-        next();
-      });
-    } else {
-      next();
-    }
-  });
-
-
-
-  app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/login',
-    failureRedirect: '/signup',
-    failureFlash:    false
-  }));
-
-  app.post('/login', passport.authenticate('local-login', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-    failureFlash:    false
-  }));
-
-  app.get('/profile/catalog/', (req, res) => {
-    connection.query('SELECT * FROM products;',(err, rows) => {
-      if (err) return err;
-      res.json(rows).end();
-    });
-  });
-// {"productID":2,"productTitle":"shulala","productPrice":100,"productDescription":"huiak"}
-  app.post('/profile/catalog', (req, res) => {
-    let flag = false;
-    let i;
-    let recordToPost = req.body;
-    connection.query('SELECT productID FROM products;',(err, rows) => {
-      if (err) {
-        return err;
-      }
-      for (i = 0; i < rows.length; i++) {
-        if (recordToPost.productID === rows[i].productID) {
-          flag = true;
-          break;
-        }
-      }
-      if ( true === flag) {
-        res.json({operation: 'POST', status: 'fail',reason: 'existing ID'}).end();
-        flag = false;
-      } else {
-        connection.query(`INSERT INTO products (productID,productTitle,productPrice,productDescription) values ('${recordToPost.productID}','${recordToPost.productTitle}','${recordToPost.productPrice}','${recordToPost.productDescription}')`, (insertError) => {
-          if (insertError) {
-            console.log(insertError);
-            return insertError;
-          }
-          res.json({operation: 'POST',status: 'success'});
-        });
-      }
-    });
-  });
-
-  app.get('/profile/catalog/*', (req, res) => {
-    let idToGet = req.params[0];
-    connection.query('SELECT * FROM products WHERE productID = ?', [idToGet], (selectError, rows) => {
-      if (selectError) {
-        return selectError;
-      }
-      res.json(rows).end();
-    });
-  });
-
-  app.delete('/profile/catalog/*', (req, res) => {
-    let idToDelete = req.params[0];
-    connection.query('DELETE FROM products WHERE productID = ?', [idToDelete], (err) => {
-      if (err) {
-        return err;
-      }
-      res.json({operation: 'DELETE', status: 'success'});
-    });
-  });
-
-  app.put('/profile/catalog/*', (req, res) => {
-    let recordToPut = req.body;
-    let idToPut = req.params[0];
-    connection.query(`UPDATE products SET productID = '${recordToPut.productID}', productTitle = '${recordToPut.productTitle}',productPrice='${recordToPut.productPrice}',productDescription='${recordToPut.productDescription}' WHERE productID = ?;`,[idToPut], (err, rows) => {
-      if (err) {
-        return err;
-      }
-      if (0 === rows.affectedRows) {
-        res.json({operation: 'PUT',status: 'FAIL',reason: 'Can not update product with given ID. No such product found.'});
-      } else {
-        res.json({operation: 'PUT',status: 'SUCCESS'});
-      }
-      res.end();
-    });
-  });
-
-
-  app.get('/logout', (req, res) => {
->>>>>>> aa025684d765ef9a3098099971ff3f28397cd6f8
     req.session.destroy();
     req.logout();
     res.redirect('/');
   });
 
-<<<<<<< HEAD
   router.post( '/login', passport.authenticate('user-login', {
     successRedirect: '/admin_profile',
     failureRedirect: '/login',
@@ -362,13 +223,12 @@ module.exports = (app, passport) => {
   router.post('/signup', passport.authenticate('local-signup', {
     successRedirect: '/login',
     failureRedirect: '/signup',
-    failureFlash:    false
-=======
-  app.get('/auth/facebook', passport.authenticate('facebook-login', {scope: 'email'}));
+    failureFlash:    false,
+  }));
+  router.get('/auth/facebook', passport.authenticate('facebook-login', {scope: 'email'}));
 
-  app.get('/auth/facebook/callback', passport.authenticate('facebook-login', {
+  router.get('/auth/facebook/callback', passport.authenticate('facebook-login', {
     successRedirect: '/profile',
     failureRedirect: '/'
->>>>>>> aa025684d765ef9a3098099971ff3f28397cd6f8
   }));
 };
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const LocalStrategy = require('passport-local').Strategy;
-<<<<<<< HEAD
 const Model = require('./models/model.js');
 const encryption = require('./passwordEncryption.js');
 const passwordCheck = require('./passwordCheck.js');
@@ -10,19 +9,11 @@ const userPromise = Model.User;
 
 module.exports = (passport) => {
 
-=======
-const FacebookStrategy = require('passport-facebook').Strategy;
-const bcrypt = require('bcrypt-nodejs');
-const connection = require('./db');
-
-module.exports = (passport) => {
->>>>>>> aa025684d765ef9a3098099971ff3f28397cd6f8
   passport.serializeUser( (user, done) => {
     done(null, user);
   });
 
   passport.deserializeUser( (user, done) => {
-<<<<<<< HEAD
     done(null, user);
   });
   const optionsOverride = {
@@ -76,104 +67,4 @@ module.exports = (passport) => {
         });
     })
   );
-=======
-    // connection.query('SELECT * FROM USERS WHERE username = ? ', [user], (err, rows) => {
-    done(null, user);
-  });
-
-  passport.use('local-signup', new LocalStrategy({
-    usernameField:     'email',
-    passportField:     'password',
-    passReqToCallback: true
-  },
-  (req, email, password, done) => {
-
-    connection.query('SELECT * FROM users WHERE username = ?', [email], (err, rows) => {
-      if (err) {
-        return done(err);
-      }
-      if (rows.length) {
-        return done(null, false);
-      } else {
-        let salt = bcrypt.genSaltSync(5);
-        let securedPassword = bcrypt.hashSync(password, salt);
-        let newUser = {
-          username: email,
-          password: securedPassword
-        };
-        connection.query(`INSERT INTO users (username, password) VALUES ('${newUser.username}','${securedPassword}');`, (insertError) => {
-          if (insertError) {
-            return done(insertError);
-          }
-          return done(null, newUser);
-        });
-      }
-    });
-  }));
-
-  passport.use('local-login', new LocalStrategy({
-    usernameField:     'email',
-    passportField:     'password',
-    passReqToCallback: true
-  },
-  (req, email, password, done) => {
-    connection.query('SELECT * FROM users WHERE username = ?', [email], (err, rows) => {
-      if (err) {
-        return done(err);
-      }
-
-      /*
-      The purpose of a verify callback is to find the user that possesses a set of credentials.
-      If the credentials are not valid (for example, if the password is incorrect),
-      done should be invoked with false instead of a user to indicate an authentication failure.
-      */
-      if (!rows.length) {
-        return done(null, false);
-      }
-      if (!bcrypt.compareSync(password, rows[0].password)) {
-        return done(null, false);
-      }
-      /*
-      If the credentials are valid, the verify callback invokes done to supply Passport with the user that authenticated.
-      */
-      req.session.user = JSON.stringify(rows[0]);
-      return done(null, rows[0]);
-    });
-  })
-);
-
-  passport.use('facebook-login', new FacebookStrategy({
-    clientID:     '************',
-    clientSecret: '*************',
-    callbackURL:  'http://localhost:3000/auth/facebook/callback'
-  },
-  (token, refreshToken, profile, done) => {
-    process.nextTick( () => {
-      connection.query('SELECT * FROM fbAuth WHERE fbId = ?', [profile.id], (selectError, selectedUser) => {
-        if (selectError) {
-          return done(selectError);
-        }
-        if (selectedUser) {
-          console.log('intra aici! CUM?!');
-          return done(null, selectedUser);
-        } else {
-          let newUser = {
-            id:    profile.id,
-            token: token,
-            name:  profile.name.givenName + ' ' + profile.name.familyName,
-            email: profile.emails[0].value
-          };
-          connection.query(`INSERT INTO fbAuth (fbId, fbToken, fbName, fbEmail) VALUES ('${newUser.id}','${newUser.token}','${newUser.name}','${newUser.email}');`, (insertError) => {
-            if (insertError) {
-              return done(insertError);
-            }
-            return done(null, newUser);
-          });
-        }
-      });
-    });
-  }
-));
-
->>>>>>> aa025684d765ef9a3098099971ff3f28397cd6f8
 };
